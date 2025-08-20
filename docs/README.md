bf4e4605de5893d112aafd25e1b74543da3b6ba7
weijie <weijiewang227@gmail.com>
2025-08-20 22:22:27 +0800

    Changed name and changed greeting message

--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,4 @@
-# Duke User Guide
-
-// Update the title above to match the actual product name
-
+# YapperBot User Guide
 // Product screenshot goes here
 
 // Product intro goes here
---
+++ src/main/java/YapperBot.java
@@ -0,0 +1,13 @@
+public class YapperBot {
+    public static void main(String[] args) {
+        String logo = """
+                --------------------------------
+                Hello! I'm YapperBot
+                What can I do for you?
+                --------------------------------
+                Bye. Hope to see you again soon!
+                --------------------------------
+                """;
+        System.out.println("Hello from\n" + logo);
+    }
+}
