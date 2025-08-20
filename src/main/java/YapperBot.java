47c0c9db9343f9cbe1084f487a673c8e72092084
weijie <weijiewang227@gmail.com>
2025-08-20 22:48:31 +0800

    Added Echoing logic

--- src/main/java/YapperBot.java
+++ src/main/java/YapperBot.java
@@ -1,13 +1,28 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class YapperBot {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String greeting = """
                 --------------------------------
                 Hello! I'm YapperBot
                 What can I do for you?
                 --------------------------------
+                """;
+        String exit = """
+                --------------------------------
                 Bye. Hope to see you again soon!
                 --------------------------------
                 """;
         System.out.println(greeting);
+        while (true) {
+            String userInput = sc.nextLine();
+            if (Objects.equals(userInput, "bye")) {
+                System.out.println(exit);
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
     }
 }
