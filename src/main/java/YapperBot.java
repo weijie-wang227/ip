6d5923a24bcb3f9cae8c35d02c5b4ab0038b4fa0
weijie <weijiewang227@gmail.com>
2025-08-20 23:17:08 +0800

    Added Add and List

--- src/main/java/YapperBot.java
+++ src/main/java/YapperBot.java
@@ -1,5 +1,7 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 public class YapperBot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,14 +16,20 @@ public class YapperBot {
                 Bye. Hope to see you again soon!
                 --------------------------------
                 """;
+        List<String> previous = new ArrayList<>();
         System.out.println(greeting);
         while (true) {
             String userInput = sc.nextLine();
             if (Objects.equals(userInput, "bye")) {
                 System.out.println(exit);
                 break;
+            } else if (Objects.equals(userInput, "list")) {
+                for (int i = 0; i < previous.size(); i++) {
+                    System.out.println((i + 1) + ", " + previous.get(i));
+                }
             } else {
-                System.out.println(userInput);
+                previous.add(userInput);
+                System.out.println("added: " + userInput);
             }
         }
     }
