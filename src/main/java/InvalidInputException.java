33a6845271bc97601757d96c34a403b453f406e4
weijie <weijiewang227@gmail.com>
2025-08-21 22:19:00 +0800

    Added new error

---
+++ src/main/java/InvalidInputException.java
@@ -0,0 +1,10 @@
+public class InvalidInputException extends RuntimeException {
+
+    public InvalidInputException() {
+        super("Sorry, I don't know what you mean :(");
+    }
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
