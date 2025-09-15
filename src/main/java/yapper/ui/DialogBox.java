f1e37b2baa5027be059b397f1e7680e9087dd5a6
weijie <weijiewang227@gmail.com>
2025-09-15 13:34:50 +0800

    Fixed Images in dialog box

--- src/main/java/yapper/ui/DialogBox.java
+++ src/main/java/yapper/ui/DialogBox.java
@@ -10,16 +10,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 /**
- * Represents a dialog box consisting a label containing text from the speaker.
+ * Represents a dialog box consisting of an ImageView to represent the speaker's face
+ * and a label containing text from the speaker.
  */
 public class DialogBox extends HBox {
     @FXML
     private Label dialog;
+    @FXML
+    private ImageView displayPicture;
 
-    private DialogBox(String text) {
+    private DialogBox(String text, Image img) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
             fxmlLoader.setController(this);
@@ -30,6 +35,7 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
+        displayPicture.setImage(img);
     }
 
     /**
@@ -40,15 +46,14 @@ public class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
-        dialog.getStyleClass().add("reply-label");
     }
 
-    public static DialogBox getUserDialog(String text) {
-        return new DialogBox(text);
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
     }
 
-    public static DialogBox getYapperDialog(String text) {
-        var db = new DialogBox(text);
+    public static DialogBox getYapperDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
         db.flip();
         return db;
     }
